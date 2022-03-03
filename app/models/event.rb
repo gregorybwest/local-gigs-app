2340f736ff754276f9d906866becf82e338bf29f
greg <gregorybwest@gmail.com>
2022-03-02 21:38:03 -0800

    yelp_venue method, parses venue data

--- app/models/event.rb
+++ app/models/event.rb
@@ -1,3 +1,15 @@
 class Event < ApplicationRecord
   belongs_to :user
+  validates_presence_of :show_time
+
+  def yelp_venue
+    response = HTTP.auth("Bearer #{Rails.application.credentials.yelp_api_key}").get("https://api.yelp.com/v3/businesses/#{yelp_venue_id}")
+    venue = response.parse(:json)
+    return {
+      id: venue["id"],
+      name: venue["name"],
+      location: venue["location"]["display_address"],
+      coordinates: venue["coordinates"]
+    }
+  end
 end
