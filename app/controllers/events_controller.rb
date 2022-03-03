47aae9fe5b0f6e5df1ad3b8ed7f3785969a2bffb
greg <gregorybwest@gmail.com>
2022-03-02 21:33:50 -0800

    two sad paths for update action

--- app/controllers/events_controller.rb
+++ app/controllers/events_controller.rb
@@ -12,7 +12,7 @@ class EventsController < ApplicationController
       yelp_venue_id: params[:yelp_venue_id], 
       show_time: params[:show_time], 
       flier_image_url: params[:flier_image_url], 
-      user_id: params[:user_id] 
+      user_id: current_user.id
     )
     if event.save
       render json: { message: "Event created successfully"}, status: :created
@@ -31,7 +31,9 @@ class EventsController < ApplicationController
     event.yelp_venue_id = params[:yelp_venue_id] || event.yelp_venue_id
     event.show_time = params[:show_time] || event.show_time
     event.flier_image_url = params[:flier_image_url] || event.flier_image_url
-    event.user_id = params[:event_id] || event.user_id
+    if event.user_id != current_user.id
+      return render json: {errors: event.errors.full_messages}, status: :unauthorized
+    end
     if event.save
       render json: event
     else
