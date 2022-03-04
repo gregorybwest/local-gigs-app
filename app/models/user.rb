eee8fc64ac76e88077e9a5285f1c28dbade0d995
greg <gregorybwest@gmail.com>
2022-03-04 09:10:10 -0800

    validates user location

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,5 +2,6 @@ class User < ApplicationRecord
   has_secure_password
   validates :email, presence: true, uniqueness: true
   validates :user_name, presence: true, uniqueness: true
+  validates :location, presence: true
   has_many :events
 end
