d566c876c01dd42034ba6b5bd0b5ec6c590ef8ca
greg <gregorybwest@gmail.com>
2022-03-03 23:17:15 -0800

    validates for user_name presence and uniqueness

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,6 @@
 class User < ApplicationRecord
   has_secure_password
   validates :email, presence: true, uniqueness: true
+  validates :user_name, presence: true, uniqueness: true
   has_many :events
 end
