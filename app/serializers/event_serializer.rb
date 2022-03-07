d0c01b21434c06ca7cd37003dfe49d1d2dd7b923
greg <gregorybwest@gmail.com>
2022-03-06 22:30:15 -0800

    owner attributes and method

--- app/serializers/event_serializer.rb
+++ app/serializers/event_serializer.rb
@@ -1,5 +1,11 @@
 class EventSerializer < ActiveModel::Serializer
   attributes :id, :show_time, :flier_image_url, :yelp_venue
 
+  attribute :owner, if: :current_user
+
+  def owner
+    current_user.id == object.user_id
+  end
+
   belongs_to :user
 end
