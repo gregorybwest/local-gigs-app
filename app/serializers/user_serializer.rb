6ecbd92200dc895a989ead9af76847cac82cb5cb
greg <gregorybwest@gmail.com>
2022-03-06 22:31:49 -0800

    adds has_many events

--- app/serializers/user_serializer.rb
+++ app/serializers/user_serializer.rb
@@ -1,5 +1,6 @@
 class UserSerializer < ActiveModel::Serializer
-  attributes :id, :email, :user_name, :image_url, :is_artist, :spotify_link, :location, :bio
+  
+  attributes :id, :user_name, :image_url, :is_artist, :spotify_link, :location, :bio
 
   has_many :events
 end
