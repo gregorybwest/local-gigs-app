0dcdccbd96f8fa9f635c7cdf574d58e7821f9320
greg <gregorybwest@gmail.com>
2022-03-06 22:29:08 -0800

    hard codes city in yelp venue search

--- app/controllers/venues_controller.rb
+++ app/controllers/venues_controller.rb
@@ -1,6 +1,7 @@
 class VenuesController < ApplicationController
+  # hard code city for MVP, user only passes in venue name in search
   def index
-    response = HTTP.auth("Bearer #{Rails.application.credentials.yelp_api_key}").get("https://api.yelp.com/v3/businesses/search?location=#{params[:location]}&categories=#{params[:categories]}&term=#{params[:term]}")
+    response = HTTP.auth("Bearer #{Rails.application.credentials.yelp_api_key}").get("https://api.yelp.com/v3/businesses/search?location=losangeles&categories=musicvenues,bars&term=#{params[:term]}")
     render json: response.parse(:json)["businesses"]
   end
 end
