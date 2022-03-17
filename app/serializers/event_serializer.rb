a4fc83aeb97f5b7cef9fc6f985ac209d29a1bb76
greg <gregorybwest@gmail.com>
2022-03-16 17:02:37 -0700

    readable_data method

--- app/serializers/event_serializer.rb
+++ app/serializers/event_serializer.rb
@@ -1,11 +1,19 @@
 class EventSerializer < ActiveModel::Serializer
-  attributes :id, :show_time, :flier_image_url, :yelp_venue
+  attributes :id, :show_time, :flier_image_url, :yelp_venue, :readable_date
 
   attribute :owner, if: :current_user
+
+  def readable_date
+    object.show_time.to_fs(:long_ordinal)
+  end
+
+
 
   def owner
     current_user.id == object.user_id
   end
+  
+  
 
   belongs_to :user
 end
