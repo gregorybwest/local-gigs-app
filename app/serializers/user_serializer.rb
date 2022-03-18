1e76dc212b6e4256190afcc6d1b011d7f21beb4b
greg <gregorybwest@gmail.com>
2022-03-18 11:19:11 -0700

    events method orders events by show_time acsending

--- app/serializers/user_serializer.rb
+++ app/serializers/user_serializer.rb
@@ -3,4 +3,9 @@ class UserSerializer < ActiveModel::Serializer
   attributes :id, :user_name, :image_url, :is_artist, :spotify_link, :location, :bio
 
   has_many :events
+
+  def events
+    object.events.order(:show_time)
+  end
+
 end
