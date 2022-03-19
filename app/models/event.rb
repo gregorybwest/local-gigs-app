9e00090b59f9f4026be90e4d042f4b62b01e21e1
greg <gregorybwest@gmail.com>
2022-03-19 16:57:19 -0500

    enables caching of event venue data

--- app/models/event.rb
+++ app/models/event.rb
@@ -3,14 +3,17 @@ class Event < ApplicationRecord
   validates_presence_of :yelp_venue_id, :show_time
 
   def yelp_venue
-    response = HTTP.auth("Bearer #{Rails.application.credentials.yelp_api_key}").get("https://api.yelp.com/v3/businesses/#{yelp_venue_id}")
-    venue = response.parse(:json)
-    return {
-      id: venue["id"],
-      name: venue["name"],
-      location: venue["location"]["display_address"],
-      coordinates: venue["coordinates"]
-    }
+    Rails.cache.fetch([self, :yelp_venue_id], expires_in: 1.day) do
+      response = HTTP.auth("Bearer #{Rails.application.credentials.yelp_api_key}").get("https://api.yelp.com/v3/businesses/#{yelp_venue_id}")
+
+      venue = response.parse(:json)
+      {
+        id: venue["id"],
+        name: venue["name"],
+        location: venue["location"]["display_address"],
+        coordinates: venue["coordinates"]
+      }
+    end
   end
 
   scope :has_not_happened, -> { where('show_time > ?', DateTime.now) }
